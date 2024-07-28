66269652c4c333a10c844f47dbf6b24204f8dd41
Anna <iaanya27@gmail.com>
2024-07-28 18:54:24 +0300

    added styles css

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,9 +1,9 @@
-
+import styles from "./Description.module.css"
 export const Description = () => {
   return (
-   <div>
-        <h1>Sip Happens Café </h1>
-        <p>Please leave your feedback about our service by selecting one of the options below.
+   <div className={styles.card}>
+        <h1 className={styles.title}>Sip Happens Café </h1>
+        <p className={styles.text}>Please leave your feedback about our service by selecting one of the options below.
 </p>
       </div>
   )
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,6 +2,14 @@ import styles from "./Feedback.module.css"
 
 export const Feedback = () => {
   return (
-    <div className={styles.card}>Feedback</div>
+    <div className={styles.card}>
+      <ul className={styles.list}>
+        <li className={styles.item}>Good: 0</li>
+        <li className={styles.item}>Neutral: 0</li>
+        <li className={styles.item}>Bad: 0</li>
+        <li className={styles.item}>Total: 0</li>
+        <li className={styles.item}>Positsve: 0</li>
+      </ul>
+    </div>
   )
 }
